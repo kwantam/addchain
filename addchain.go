47a828795eefaf3adf18530618b9775095c3c876
kwantam <kwantam@gmail.com>
2018-03-19 11:57:34 -0700

    windowing for bos-coster

--- addchain.go
+++ addchain.go
@@ -151,24 +151,93 @@ func sequence(chn []*big.Int) {
     }
 }
 
-/*
 func bits(x *big.Int) {
-    var bitnum = x.BitLen() - 1
-    for ; bitnum >= 0; bitnum-- {
+    for bitnum := x.BitLen() - 1; bitnum >= 0; bitnum-- {
         if x.Bit(bitnum) > 0 {
-            print("1 ")
+            print("1")
         } else {
-            print("0 ")
+            print("0")
         }
     }
     print("\n")
 }
 
-func window(x *big.Int, len int) {
-    var in_window = false
-    var bitnum = x.BitLen()
+type winT struct {
+    start, end int
+    in_window bool
 }
-*/
+
+func window(x *big.Int, winsize int) {
+    var in_window = false
+    var win_start = -1
+    var last_one = -1
+    var thiswinsize = 0
+    var bit = -1
+    var bitnum = x.BitLen() - 1
+    var runs = make([]winT, 0, bitnum / 4)
+    for ; bitnum >= -1; bitnum-- {
+        // dummy value for last iteration
+        if bitnum == -1 {
+            bit = -1
+        } else {
+            bit = int(x.Bit(bitnum))
+        }
+        if in_window {
+            // update last-seen 1
+            if bit > 0 {
+                last_one = bitnum
+            }
+
+            if (thiswinsize < winsize) && (bit >= 0) {
+                // continue growing the window
+                thiswinsize++
+            } else {
+                // need to end the window
+                runs = append(runs, winT{win_start, last_one, true})
+                in_window = false
+                win_start = last_one - 1
+                thiswinsize = last_one - bitnum + 1
+                last_one = -1
+                // special case when we hit the end of the number
+                if bitnum == -1 && win_start >= 0 {
+                    runs = append(runs, winT{win_start, 0, false})
+                }
+            }
+        } else {
+            if bit != 0 {
+                // valid zero-run has non-zero length and a valid start position
+                if win_start >= 0  && win_start > bitnum {
+                    runs = append(runs, winT{win_start, bitnum + 1, false})
+                }
+                in_window = true
+                win_start = bitnum
+                thiswinsize = 1
+                last_one = bitnum
+            }
+        }
+    }
+
+    // print out a graphical representation of the window
+    bits(x)
+    for _, run := range runs {
+        if run.in_window {
+            if run.start == run.end {
+                print("|")
+            } else {
+                print(">")
+                for i := 0; i < run.start - run.end - 1; i++ {
+                    print("-")
+                }
+                print("<")
+            }
+        } else {
+            for i := 0; i < run.start - run.end + 1; i++ {
+                print(" ")
+            }
+        }
+    }
+    print("\n")
+}
 
 func usage() {
     fmt.Printf("Usage: %s <formula>\n", os.Args[0])
@@ -187,6 +256,6 @@ func main() {
         os.Exit(-1)
     }
 
-    //bits(q)
-    sequence(minchain(q))
+    window(q, 5)
+    //sequence(minchain(q))
 }
